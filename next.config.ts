62783e6b8ddb3119326c864e340ac3ce8dd15dd3
pulkit1417 <gupta.pulkit2408@gmail.com>
2025-12-12 16:01:59 +0530

    event page done

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '/**', 
+      },
+    ],
   },
 };
 
